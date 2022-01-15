3fe57e376f13600063974c58fcaa1f4467050111
Kaynite <kudo.200200@gmail.com>
2022-01-15 17:08:55 +0200

    Added custom directive for icons

---
+++ src/directives/icon.js
@@ -0,0 +1,21 @@
+export default {
+  beforeMount(el, binding) {
+    let className = `fa fa-${binding.value}`;
+
+    if (binding.arg == "custom") {
+      className = binding.value;
+    }
+
+    if (binding.modifiers.right) {
+      className += " float-right";
+    }
+
+    if (binding.modifiers.yellow) {
+      className += " text-yellow-500";
+    } else {
+      className += " text-green-500";
+    }
+
+    el.innerHTML += `<i class="${className}"></i>`;
+  },
+};
